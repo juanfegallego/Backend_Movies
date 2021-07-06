bc522561dbf6ea2723af273c45ec34f4f7d40cc3
juanfegallego <juanfegallegodj@gmail.com>
2021-07-06 18:58:07 +0200

    changes for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./db.js");
 const app = express();
 const router = require("./router");
-const port = 3001;
+const port = process.env.PORT || 3001;
 const cors = require("cors")
 
 
